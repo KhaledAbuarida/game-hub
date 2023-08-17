cea845922b14f876fd987e2e376e8843ff5264fd
KhaledAbuarida <khaledabuarida0@gmail.com>
2023-08-17 04:35:58 +0300

    Show Genre Loading Skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,19 +1,24 @@
 import { List, ListItem, Image, Text, HStack } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import cropImageByUrl from "../services/image-url";
+import GenreSkeleton from "./GenreSkeleton";
+
+const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function GenreList() {
-    const { data } = useGenres();
+    const { data, isLoading } = useGenres();
 
     return (
         <List paddingY={9}>
+            {isLoading && skeletons.map(s => <GenreSkeleton key={s}/>)}
             {data.map(genre => 
                 <ListItem key={genre.id}>
                     <HStack paddingY={1}>
                         <Image boxSize='32px' borderRadius={10} src={cropImageByUrl(genre.image_background)}/>
                         <Text fontSize='md'>{genre.name}</Text>
                     </HStack>
-                </ListItem> )}
+                </ListItem> 
+            )}
         </List>
     )
 }
---
+++ src/components/GenreSkeleton.tsx
@@ -0,0 +1,12 @@
+import { Card, HStack, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react"
+
+function GenreSkeleton() {
+    return (
+            <HStack paddingY={1}>
+                <Skeleton boxSize='32px' borderRadius={10}/>
+                <Skeleton height='12px' width='100px'/>
+            </HStack>
+    )
+}
+
+export default GenreSkeleton
